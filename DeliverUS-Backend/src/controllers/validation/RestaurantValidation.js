301ccaacd52e9550aa9f72046f31b3ac3f88b31f
lurio84 <115478617+lurio84@users.noreply.github.com>
2024-05-20 16:23:41 +0200

    Initialize solution

---
+++ DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
@@ -0,0 +1,86 @@
+import { check } from 'express-validator'
+import { checkFileIsImage, checkFileMaxSize } from './FileValidationHelper.js'
+const maxFileSize = 2000000 // around 2Mb
+// Solution
+import { Restaurant } from '../../models/models.js'
+
+// Solution
+const checkBussinessRuleOneRestaurantPromotedByOwner = async (ownerId, promotedValue) => {
+  if (promotedValue) {
+    try {
+      const promotedRestaurants = await Restaurant.findAll({where:{userId: ownerId, promoted: true}})
+      if (promotedRestaurants.length !== 0) {
+        return Promise.reject(new Error("You can only promote one restaurant at a time"))
+      }
+    }  
+    catch (err){
+      return Promise.reject(new Error(err))
+    }
+  }
+
+  return Promise.resolve('ok')
+}
+
+const create = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  // Solution
+  check('promoted')
+    .custom(async (value, {req}) => {
+      return checkBussinessRuleOneRestaurantPromotedByOwner(req.user.id, value)
+    })
+    .withMessage('You can only promote one restaurant at a time')
+]
+
+const update = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  // Solution
+  check('promoted')
+    .custom(async (value, {req}) => {
+      return checkBussinessRuleOneRestaurantPromotedByOwner(req.user.id, value)
+    })
+    .withMessage('You can only promote one restaurant at a time')
+]
+
+export { create, update }
